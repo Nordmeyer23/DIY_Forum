6a7d50155940fbfba27d8e4a370faed8ac80c6aa
SC\layton.nordmeyer <layton.nordmeyer@my.simpson.edu>
2016-10-18 11:34:59 -0500

    added deploy script

--- DIY_FORUM/public_html/deploy_script.php
+++ DIY_FORUM/public_html/deploy_script.php
@@ -1,7 +1,18 @@
-<p>Deploy script</p>
-<pre>
-<?php 
-$output = shell_exec("cd ..;hg pull;hg update 2>&1");
+<!DOCTYPE html>
+
+<html lang="en">
+	<head>
+		<title>Deployment Script</title>
+	</head>
+	<body>
+	<p>Deploy script</p>
+		<pre>
+		
+<?php
+$output = shell_exec("cd ..;git pull 2>&1");
 echo $output;
 ?>
-</pre>
+		
+		</pre>
+	</body>
+</html>
